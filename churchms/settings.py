7193b7c3794885eaf8cf3333541b97f86ac9e294
sama4ga <mauriceoscar58@gmail.com>
2024-01-30 03:32:09 +0100

    Removed unneccessary plugins and update logging

--- churchms/settings.py
+++ churchms/settings.py
@@ -142,21 +142,33 @@ CONN_HEALTH_CHECKS = True
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-      "formatters": {
-          "verbose": {
-              "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
-              "style": "{",
-          },
-          "simple": {
-              "format": "{levelname} {message}",
-              "style": "{",
-          },
-      },
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+    "filters": {
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
     "handlers": {
         "file": {
             "level": os.environ.get("DJANGO_LOG_LEVEL"),
             "class": "logging.FileHandler",
             "filename": os.path.join(BASE_DIR, "logs/debug.log"),
+            "formatter": "verbose",
+        },
+        "console": {
+            "level": "INFO",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
         },
     },
     "loggers": {
